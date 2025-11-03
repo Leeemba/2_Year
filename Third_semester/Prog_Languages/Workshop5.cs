9a1c0d3d60945c39a25481c9c0663c14d53d5bde
MaxBaukin <408230@niuitmo.ru>
2025-11-04 00:39:37 +0300

    Add files via upload

---
+++ Third_semester/Prog_Languages/Workshop5.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Workshop5
+{
+    static async Task Main()
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        var files = Directory.GetFiles("D:/yDownloads/LAB5");
+
+        var tasks = files.Select(async file =>
+        {
+            var text = await File.ReadAllTextAsync(file);
+            var wordCount = text
+                .Split([' ', '\n', '\r', '\t', ',', '.', '!', '?', ';', ':', '"'],
+                       StringSplitOptions.RemoveEmptyEntries)
+                .Length;
+
+            return $"{Path.GetFileName(file)} → {wordCount} слов";
+        });
+
+        await foreach (var result in Task.WhenEach(tasks))
+        {
+            Console.WriteLine(await result);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"\nВремя выполнения: {stopwatch.ElapsedMilliseconds} мс");
+    }
+}
